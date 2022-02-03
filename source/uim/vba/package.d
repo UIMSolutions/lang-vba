50b2ec43c223eefcb728b63d874b6bdd9dfc6ae7
UIM GIT-Master <ui.manufaktur@gmail.com>
2022-02-03 20:37:29 +0100

    Adding funcions

--- source/uim/vba/package.d
+++ source/uim/vba/package.d
@@ -1,5 +1,11 @@
 module uim.vba;
 
+public import std.stdio;
+public import std.string;
+
+public import uim.core;
+public import uim.oop;
+
 public import uim.vba.access;
 public import uim.vba.classes;
 public import uim.vba.excel;
